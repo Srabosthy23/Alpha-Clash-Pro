36f57aadcdddbb9ea442b1bc69c5576f5b594be3
Srabosthy23e <srabosthycse@gmail.com>
2024-02-16 12:29:14 +0600

    set newscore and newlife

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,6 +6,53 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event){
+    // player pressed key
+    const playerPressed = event.key;
+
+    // expected key
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();    
+    console.log(expectedAlphabet, playerPressed)
+
+    // checked
+    if(playerPressed === expectedAlphabet){
+        console.log("win");
+        // get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+         
+        // set new score
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+
+        removeBackgroundColorById(expectedAlphabet)
+        continueGame();
+    }
+    else{
+        // get current life
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // reduce currentlife
+        const newLife = currentLife - 1;
+        currentLifeElement.innerText = newLife;
+
+        if(newLife === 0){
+            console.log("fuck")
+            gameOver();
+        }
+    }
+
+    
+}
+// capture keyboard key-press
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+// playing game / continue game
 function continueGame(){
     const alphabet = getARandomALphabet();
     
@@ -18,6 +65,12 @@ function continueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('score')
     showElementById('playground');
     continueGame()
 }   
+
+function gameOver(){
+    hideElementById('playground')
+    showElementById('score');
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,16 +1,25 @@
+// hide element
 function hideElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
+// show element
 function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
+// set background color
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
+}
+
+// remove background color
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400');
 }
 
 function getARandomALphabet(){
